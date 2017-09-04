ea8b435ce1e77cecc3e87716d77135229eb31725
lorne <2270333671@qq.com>
2017-09-04 17:25:31 +0800

    支付跳转

--- src/pages/PaySuccess.js
+++ src/pages/PaySuccess.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 import '../styles/Pay.css'
 
+
 export default class PaySuccess extends Component {
-    render(){
-        return(
-            <p>
+
+    componentDidMount() {
+        setTimeout(function () {
+            window.postMessage('pay-success', window.location.href)
+        }, 100)
+
+    }
+
+
+    render() {
+        return ( <p>
                 pay-success works!
             </p>
         )
